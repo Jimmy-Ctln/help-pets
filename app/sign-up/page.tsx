ef89bc254f824e530d08d081ccf40ce2d6f9697f
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-09-15 21:56:18 +0200

    feat: Update login and add sign-up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,15 +2,8 @@
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import loginDog from '@/public/assets/loginDog.jpg';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import loginCat from '@/public/assets/loginCat.jpg';
 import Image from 'next/image';
 import {} from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
@@ -20,35 +13,44 @@ import Link from 'next/link';
 export default function Login() {
   return (
     <div className="mt-28 flex justify-center items-center">
-      <Card className="w-[80%]">
-        <div className="flex">
-          <div className="w-1/2 flex items-center justify-center">
+      <Card className="w-[80%] min-h-full">
+        <div className="flex h-full">
+          <div className="w-1/2 flex items-stretch">
             <Image
-              src={loginDog}
+              src={loginCat}
               alt="Photo d'un chat pour la connexion"
               className="rounded-l-lg object-cover h-full"
+              style={{ height: '100%', width: '100%' }}
             />
           </div>
           <div className="w-1/3 mx-auto flex flex-col justify-center py-6">
             <CardHeader>
-              <CardTitle className="mx-auto text-2xl">Connexion</CardTitle>
+              <CardTitle className="mx-auto text-2xl">Créer un compte</CardTitle>
             </CardHeader>
             <CardContent>
               <form>
                 <div className="grid w-full mt-4 items-center gap-4">
                   <div className="flex flex-col space-y-1.5 w-full mx-auto">
-                    <Label htmlFor="email">Email</Label>
+                    <Label>Association</Label>
+                    <Input
+                      id="association"
+                      type="text"
+                      placeholder="Entrez le nom de votre association"
+                    />
+                    <Label className="mt-2" htmlFor="email">
+                      Email
+                    </Label>
                     <Input id="email" type="email" placeholder="Entrez votre email" />
                     <Label className="mt-2" htmlFor="password">
                       Mot de passe
                     </Label>
                     <Input id="password" type="password" placeholder="Entrez votre mot de passe" />
                   </div>
-                  <Button className="w-full mx-auto mt-2">Se connecter</Button>
+                  <Button className="w-full mx-auto mt-2">Créer un compte</Button>
                 </div>
                 <div className="relative my-4 flex items-center justify-center overflow-hidden">
                   <Separator />
-                  <div className="px-2 text-center bg-background text-sm">OU</div>
+                  <div className="px-2 text-center bg-background text-sm">OR</div>
                   <Separator />
                 </div>
                 <Button className="mx-auto w-full" variant="outline">
@@ -64,14 +66,14 @@ export default function Login() {
                   >
                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
                   </svg>
-                  Se connecter avec Google
+                  Créer un compte avec Google
                 </Button>
               </form>
             </CardContent>
             <CardFooter className="mt-4 flex text-xs mx-auto">
-              <span>Aucun compte ?&nbsp;</span>
-              <Link href={'sign-up'} className="text-blue-500">
-                Créer un compte
+              <span>Vous avez déjà un compte ?&nbsp;</span>
+              <Link href={'login'} className="text-blue-500">
+                Se connecter
               </Link>
             </CardFooter>
           </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,34 +3,41 @@ import logo from '@/public/assets/logo.svg';
 import Image from 'next/image';
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import { Home, LogIn } from 'lucide-react';
 
 export default function Header() {
-  const navigation = [
-    { section: 'Accueil', href: '/' },
-    { section: 'Se connecter', href: '/login' },
+  const navigationMenuItems = [
+    { title: 'Accueil', href: '/', icon: Home },
+    { title: 'Se connecter', href: '/login', icon: LogIn },
   ];
 
   return (
-    <header className="w-full mt-2">
+    <header className="w-full mt-4 px-20">
       <div className="rounded-full border-2 p-4">
         <div className="flex items-center justify-between">
-          <div className="flex gap-2 items-end">
-            <Image src={logo} alt="logo" width={30} height={'30'} />
-            <h2 className="text-xl font-bold gap-2">Help Pets</h2>
+          <div className="flex gap-2 items-center">
+            <Image src={logo} className="object-cover" alt="logo" width={50} height={50} />
+            <h3 className="text-xl font-bold gap-2">
+              <Link href={'/'}>Main dans la Patte</Link>
+            </h3>
           </div>
           <NavigationMenu>
             <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Accueil</NavigationMenuTrigger>
-              </NavigationMenuItem>
+              {navigationMenuItems.map((item) => (
+                <NavigationMenuItem key={item.title}>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                    <Link href={item.href} className="flex-row items-center gap-2.5">
+                      <item.icon className="h-5 w-5 shrink-0" />
+                      {item.title}
+                    </Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
             </NavigationMenuList>
           </NavigationMenu>
         </div>
