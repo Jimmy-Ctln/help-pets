32d57d5e7a399e9f0ddd0e2c82296b16fbc7ef5b
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-09-17 13:19:37 +0200

    fix: Fix problem with request

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from 'next/server'
 import { UserControler } from '@/modules/user/user-controler'
-import { userResponse } from '@/modules/user/user-response'
 
 export async function POST(req: Request) {
   try {
     const { email, password } = await req.json()
 
     const controller = new UserControler()
-    const user = await controller.registerUser(email, password)
+    const data = {
+      email,
+      password,
+    }
+    const user = await controller.registerUser(data)
 
-    return NextResponse.json(userResponse(user), { status: 201 })
+    return NextResponse.json(user, { status: 201 })
   } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 400 })
   }
