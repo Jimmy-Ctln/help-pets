d2b85c72b6bace5c833b4e8abdbb9f9509e8c639
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-09-17 17:07:11 +0200

    fix: Add suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <SessionWrapper>
-      <html lang="fr" className={quicksand.className}>
+      <html lang="fr" className={quicksand.className} suppressHydrationWarning>
         <body className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1 overflow-auto">{children}</main>
